126727683b16d92b3bcaf0aa86bbd85897b00813
Mikelangelon <mikelangelon@gmail.com>
2024-06-16 21:19:45 +0200

    Allow multiple layers

--- graphics/mapscene.go
+++ graphics/mapscene.go
@@ -15,10 +15,12 @@ type MapScene struct {
 	Map            *Map
 	TileSet        *TileSet
 	Image          *ebiten.Image
-	FormattedLayer []int64
+	FormattedLayer []formattedLayer
 
 	screenWidth, screenHeight, scale int
 }
+
+type formattedLayer []int64
 
 func NewMapScene(mapImage, tmx, tsx []byte, screenWidth, screenHeight, scale int) (*MapScene, error) {
 	img, _, err := ebitenutil.NewImageFromReader(bytes.NewReader(mapImage))
@@ -38,7 +40,7 @@ func NewMapScene(mapImage, tmx, tsx []byte, screenWidth, screenHeight, scale int
 		return nil, err
 	}
 
-	formattedLayer, err := formattedLayer(m)
+	formattedLayers, err := formatLayers(m)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +48,7 @@ func NewMapScene(mapImage, tmx, tsx []byte, screenWidth, screenHeight, scale int
 		Map:            m,
 		TileSet:        t,
 		Image:          ebitenImage,
-		FormattedLayer: formattedLayer,
+		FormattedLayer: formattedLayers,
 		screenWidth:    screenWidth,
 		screenHeight:   screenHeight,
 		scale:          scale,
@@ -59,14 +61,16 @@ func (g *MapScene) Update() error {
 
 func (g *MapScene) Draw(screen *ebiten.Image) {
 	scaleX, scaleY := g.Scale()
-	for i, id := range g.FormattedLayer {
-		op := &ebiten.DrawImageOptions{}
-		tx := (i % g.Map.Width) * g.Map.TileWidth
-		ty := (i / g.Map.Width) * g.Map.TileHeight
-		op.GeoM.Translate(float64(tx), float64(ty))
-		op.GeoM.Scale(scaleX, scaleY)
+	for _, layer := range g.FormattedLayer {
+		for i, id := range layer {
+			op := &ebiten.DrawImageOptions{}
+			tx := (i % g.Map.Width) * g.Map.TileWidth
+			ty := (i / g.Map.Width) * g.Map.TileHeight
+			op.GeoM.Translate(float64(tx), float64(ty))
+			op.GeoM.Scale(scaleX, scaleY)
 
-		screen.DrawImage(g.tileImage(int(id-1)), op)
+			screen.DrawImage(g.tileImage(int(id-1)), op)
+		}
 	}
 }
 
@@ -91,7 +95,7 @@ func (g *MapScene) TileForPos(x, y int) Tile {
 	ry := y / g.Map.TileHeight
 
 	i := rx + ry*g.Map.Width
-	id := g.FormattedLayer[i] - 1
+	id := g.FormattedLayer[0][i] - 1
 	for _, v := range g.TileSet.Tiles {
 		if v.Id == int(id) {
 			return v
@@ -100,24 +104,26 @@ func (g *MapScene) TileForPos(x, y int) Tile {
 	return Tile{}
 }
 
-func formattedLayer(tmx *Map) ([]int64, error) {
-	if len(tmx.Layers) > 1 {
-		return nil, errors.New("only 1 layer is supported")
-	}
-	var formattedLayer []int64
-	if tmx.Layers[0].Data.Encoding != "csv" {
-		return formattedLayer, errors.New("only csv file supported")
-	}
-	ids := strings.Split(string(tmx.Layers[0].Data.Raw), ",")
-	for _, s := range ids {
-		id, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
-		if err != nil {
-			return formattedLayer, err
+func formatLayers(tmx *Map) ([]formattedLayer, error) {
+	var result []formattedLayer
+	for _, v := range tmx.Layers {
+		var formattedLayer formattedLayer
+		if v.Data.Encoding != "csv" {
+			return result, errors.New("only csv file supported")
 		}
-		if err != nil {
-			return nil, err
+		ids := strings.Split(string(v.Data.Raw), ",")
+		for _, s := range ids {
+			id, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
+			if err != nil {
+				return result, err
+			}
+			if err != nil {
+				return nil, err
+			}
+			formattedLayer = append(formattedLayer, id)
 		}
-		formattedLayer = append(formattedLayer, id)
+		result = append(result, formattedLayer)
 	}
-	return formattedLayer, nil
+
+	return result, nil
 }
